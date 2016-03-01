b1911b27ff0cf1bbf916968090d96c303bb2a774
Kalen Jordan <kalen@magemail.co>
2016-03-01 02:50:40 -0800

    Pull buzzstream feed in

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,16 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Helper\TwigHelper;
+use GoodLinks\BuzzStreamFeed\Api;
+use GoodLinks\BuzzStreamFeed\History;
+use GoodLinks\BuzzStreamFeed\HistoryItem;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        Api::setConsumerKey(getenv('BUZZSTREAM_CONSUMER_KEY'));
+        Api::setConsumerSecret(getenv('BUZZSTREAM_CONSUMER_SECRET'));
+
+        $key = isset($_GET['key']) ? $_GET['key'] : null;
+        if (!$key || $key != getenv('WEB_KEY')) {
+            die("Access denied");
+        }
+
+        $offset = isset($_GET['offset']) ? $_GET['offset'] : null;
+        $size = isset($_GET['size']) ? $_GET['size'] : 50;
+
+        $history = History::getList($offset, $size);
+
         $twig = TwigHelper::twig();
 
         return $twig->render('index.html.twig', array(
             "title"             => "BuzzStream Feed",
             "body_class"        => "home",
-1        ));
+            "history"           => $history,
+        ));
     }
 }
